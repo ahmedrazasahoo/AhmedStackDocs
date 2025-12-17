ef97a9d1bf20fc3fa488c127b9a491375a243d88
ahmedrazasahoo <7ahmed8raza6@gmail.com>
2025-12-17 17:59:29 +0500

    fixces

---
+++ src/pages/card1.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      car: {
+        car_name: "Toyota",
+        car_description: "This is a good car"
+      }
+    };
+  }
+};
+</script>
+
+<template>
+  <div>
+    <h1>Name: {{ car.car_name }}</h1>
+    <p>Description: {{ car.car_description }}</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: #333;
+  font-size: 24px;
+  margin-bottom: 10px;
+  text-align: center;
+}
+p {
+  color: #666;
+  font-size: 16px;
+  line-height: 1.5;
+  text-align: center;
+}
+</style>
